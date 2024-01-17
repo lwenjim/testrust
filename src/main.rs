a8628711f85c167fd95bd384593228d77b46b47c
liuwenjin <liuwenjin@jspp.com>
2024-01-17 23:31:04 +0800

    feat: read data from cli     named,non-named

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![allow(unused)]
+use std::env;
+use structopt::StructOpt;
+
 fn main() {
-    println!("{}", 123);
+    #[derive(Debug)]
+    // #[allow(dead_code)]
+    struct Cli {
+        pattern: String,
+        path: std::path::PathBuf,
+    }
+    let pattern = env::args().nth(1).expect("no pattern given");
+    let path = env::args().nth(1).expect("no path given");
+    let args = Cli {
+        pattern,
+        path: std::path::PathBuf::from(path),
+    };
+    println!("{:?}", args);
+
+    #[derive(Debug, StructOpt)]
+    // #[allow(dead_code)]    
+    struct Cli2 {
+        #[structopt(short = "t", long = "pattern")]        
+        pattern: String,
+        #[structopt(parse(from_os_str))]
+        #[structopt(short = "p", long = "path")]
+        path: std::path::PathBuf,
+    }
+    let args = Cli2::from_args();
+    println!("{:?}", args);
 }
