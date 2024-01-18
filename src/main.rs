a616bf32491af6e3d04e1f70b4219f2d74ded2eb
liuwenjin <liuwenjin@jspp.com>
2024-01-18 17:17:59 +0800

    feat: update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![allow(unused)]
-use std::env;
-use structopt::StructOpt;
+use std::{error::Error};
 
-fn main() {
+
+// cargo run --quiet -- --path 123 --pattern abc
+fn main()  -> Result<(), Box<dyn Error>>{
+    use std::env;
+    use structopt::StructOpt;
     #[derive(Debug)]
     // #[allow(dead_code)]
     struct Cli {
@@ -28,4 +31,35 @@ fn main() {
     }
     let args = Cli2::from_args();
     println!("{:?}", args);
+
+    use regex::Regex;
+    let result = std::fs::read_to_string("aa.txt");
+    match result {
+        Err(error) => {
+            panic!("err: {}", error)
+        }
+        Ok(content) => {
+            println!("content: {}", content)
+        }
+    }
+    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    println!("Did our date match? {}", re.is_match("2014-01-01"));
+
+    use std::{thread, time::Duration};
+    ctrlc::set_handler(move || {
+        println!("received Ctrl+C!");
+    })
+    .expect("Error setting Ctrl-C handler");
+    thread::sleep(Duration::from_secs(2));
+
+    use signal_hook::{consts::SIGINT, iterator::Signals};
+    use std::{error::Error};
+    let mut signals = Signals::new(&[SIGINT])?;
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            println!("Received signal {:?}", sig);
+        }
+    });
+    thread::sleep(Duration::from_secs(2));
+    Ok(())
 }
