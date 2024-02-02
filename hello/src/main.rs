f128ff73858cbf83ccb06ab13400e143faae2cc2
liuwenjin <liuwenjin@jspp.com>
2024-02-02 16:22:12 +0800

    feat: update hello/main.rs

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,33 +1,23 @@
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
+use std::{fs, io::prelude::*, net::TcpListener};
+use std::io::BufReader;
+use std::net::TcpStream;
+
 fn main() {
-    // 监听地址: 127.0.0.1:7878
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
+        // let response = "HTTP/1.1 200 OK\r\n\r\nok";
+        // stream.unwrap().write_all(response.as_bytes()).unwrap();
+        handle_connection(stream.unwrap());
     }
 }
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("Request: {:#?}", http_request);
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write_all(response.as_bytes()).unwrap();
 
+fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
+        ("HTTP/1.1 200 OK", "hello/hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        ("HTTP/1.1 404 NOT FOUND", "hello/404.html")
     };
     let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
