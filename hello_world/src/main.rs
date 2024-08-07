bf40ff4d3a9e352cc56ad9c916d3b0adc63b2e81
liuwenjin <liuwenjin@jspp.com>
2024-08-08 00:04:13 +0800

    feat: add macro

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(warnings, unused)]
 fn main() {
     // ===========================使用 macro_rules! 来创建宏 ============
     macro_rules! say_hello {
@@ -40,4 +41,132 @@ fn main() {
         let x = 1u32;
         x * x + 2 * x - 1
     });
+
+    // ============macro_rules -> 重载 =====
+    // 根据你调用它的方式 test 将以不同的方式来比较 $left 和 $right
+    macro_rules! test {
+        ($left:expr;and $right:expr) => {
+            println!(
+                "{:?} and {:?} is {:?}",
+                stringify!($left),
+                stringify!($right),
+                $left && $right
+            );
+        };
+        ($left:expr;or $right:expr) => {
+            println!(
+                "{:?} or {:?} is {:?}",
+                stringify!($left),
+                stringify!($right),
+                $left || $right
+            );
+        };
+    }
+    test!(1i32 + 1 == 2i32;and 2i32 * 2 == 4i32);
+    test!(true;or false);
+
+    // ============macro_rules -> 重复 =====
+    macro_rules! find_min {
+        // 基本情形
+        ($x:expr) => {
+            $x
+        };
+        ($x:expr, $($y:expr),+) => (
+            std::cmp::min($x, find_min!($($y), +))
+        )
+    }
+
+    println!("{}", find_min!(1u32));
+    println!("{}", find_min!(1u32 + 2, 2u32));
+    println!("{}", find_min!(5u32 + 2u32 * 3, 4u32));
+
+    //==================macro_rules  DSL（领域专用语言）
+    macro_rules! calculate {
+        (eval $e:expr) => {
+            {
+                let val: usize = $e;// 强制类型为整型
+                println!("{} = {}", stringify!{$e}, val);
+            }   
+        };
+    }
+    calculate!(
+        eval 1 + 2 //看到了吧 eval 看并不是Rust 的关键字
+    );
+
+    calculate!(
+        eval (1 + 2) * (3 / 4)
+    );
+
+    // ===================macro_rules 可变参数接口
+    macro_rules! calculate2 {
+        // 单个eval 模式
+        (eval $e:expr) => (
+            let val: usize = $e;
+            println!("{} = {}", stringify!($e), val);            
+        );
+
+        // 递归拆解多重 eval
+        (eval $e:expr, $(eval $es:expr), +) => {{
+            calculate2! {eval $e}
+            calculate2! {$(eval $es), +}
+        }};
+    }
+    calculate2!(
+        eval 1 + 2,
+        eval 3 + 4,
+        eval (2 * 3) + 1
+
+    )
+}
+// ==================macro_rules -> DRY (不写重复代码)=========
+use std::ops::{Add, Mul, Sub};
+macro_rules! assert_equal_len {
+    ($a:ident, $b: ident, $func:ident, $op:tt) => {
+        assert!(
+            $a.len() == $b.len(),
+            "{:?}: dimension mismatch: {:?} {:?} {:?}",
+            stringify!($func),
+            ($a.len(),),
+            stringify!($op),
+            ($b.len(),)
+        );
+    };
+}
+
+macro_rules! op {
+    ($func:ident, $bound:ident, $op:tt, $method:ident) => {
+        fn $func<T: $bound<T, Output = T> + Copy>(xs: &mut Vec<T>, ys: &Vec<T>) {
+            assert_equal_len!(xs, ys, $func, $op);
+
+            for (x, y) in xs.iter_mut().zip(ys.iter()) {
+                *x = $bound::$method(*x, *y);
+            }
+        }
+    };
+}
+op!(add_assign, Add, +=, add);
+op!(mul_assign, Mul, *=, mul);
+op!(sub_assign, Sub, -=, sub);
+
+mod test {
+    use std::iter;
+    macro_rules! test {
+        ($func:ident, $x:expr, $y:expr,$z:expr) => {
+            #[test]
+            fn $func() {
+                for size in 0usize..10 {
+                    let mut x: Vec<_> = iter::repeat($x).take(size).collect();
+                    let y: Vec<_> = iter::repeat($y).take(size).collect();
+                    let z: Vec<_> = iter::repeat($z).take(size).collect();
+
+                    super::$func(&mut x, &y);
+
+                    assert_eq!(x, z);
+                }
+            }
+        };
+    }
+    test!(add_assign, 1u32, 2u32, 3u32);
+    test!(mul_assign, 2u32, 3u32, 6u32);
+    test!(sub_assign, 3u32, 2u32, 1u32);
 }
