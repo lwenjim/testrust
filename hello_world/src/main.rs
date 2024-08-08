fe10a391394831a0a1ecf61e4eedd14b953b4508
liuwenjin <liuwenjin@jspp.com>
2024-08-09 00:10:58 +0800

    feat: error -> panic

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -86,23 +86,21 @@ fn main() {
             {
                 let val: usize = $e;// 强制类型为整型
                 println!("{} = {}", stringify!{$e}, val);
-            }   
+            }
         };
     }
     calculate!(
         eval 1 + 2 //看到了吧 eval 看并不是Rust 的关键字
     );
 
-    calculate!(
-        eval (1 + 2) * (3 / 4)
-    );
+    calculate!(eval(1 + 2) * (3 / 4));
 
     // ===================macro_rules 可变参数接口
     macro_rules! calculate2 {
         // 单个eval 模式
         (eval $e:expr) => (
             let val: usize = $e;
-            println!("{} = {}", stringify!($e), val);            
+            println!("{} = {}", stringify!($e), val);
         );
 
         // 递归拆解多重 eval
@@ -115,11 +113,223 @@ fn main() {
         eval 1 + 2,
         eval 3 + 4,
         eval (2 * 3) + 1
+    );
 
-    )
+    // ===================== 错误处理 -> panic ===================
+    fn give_princess(gift: &str) {
+        // 公主讨厌蛇, 所以如果公主表示厌恶的话我们要停止
+        // if gift == "snake" {panic!("AAA aaaa!!!!");}
+        println!("I love {}s", gift);
+    }
+    give_princess("teddy bear");
+    give_princess("snake");
+
+    // ===================== 错误处理 -> option 和 unwrap ===================
+    // 平民(commoner)们 见多识广 收到什么礼物都应对
+    // 所有礼物都是显示的实用 match 来处理
+    fn give_commoter(gift: Option<&str>) {
+        // 指出每种情况的做法
+        match gift {
+            Some("snake") => println!("Yuck I'm throwing that snake in a fire."),
+            Some(inner) => println!("{}? How nice.", inner),
+            None => println!("No gift? Oh well."),
+        }
+    }
+
+    // 养在深阖人未识得公主剪刀蛇就会 panic 恐慌
+    // 这里所有的礼物都实用 unwrap 隐是地处理
+    fn give_princess2(gift: Option<&str>) {
+        // unwrap 在接受到 None 时将返回 panic
+        // let inside = gift.unwrap();
+        // if inside == "snake" {panic!("AAAAAAA!!!!!");}
+        // println!("I love {}s !!!!!", inside);
+    }
+
+    let food = Some("chicken");
+    let snake = Some("snake");
+    let void = None;
+
+    give_princess2(food);
+    give_princess2(snake);
+    give_princess2(void);
+
+    let bird = Some("robin");
+    let nothing = None;
+
+    give_princess2(bird);
+    give_princess2(nothing);
+
+    // ===================== 错误处理 -> 使用 ? 解开 Option ===================
+    fn next_birthday(current_age: Option<u8>) -> Option<String> {
+        // 如果 current_age 是 none 这将返回none
+        // 如果 current_age 是 some 内部的 u8 将赋值给 next_age
+        let next_age: u8 = current_age?;
+        Some(format!("Next year I will be {}", next_age))
+    }
+
+    struct Person {
+        job: Option<Job>,
+    }
+
+    #[derive(Clone, Copy)]
+    struct Job {
+        phone_number: Option<PhoneNumber>,
+    }
+
+    #[derive(Clone, Copy)]
+    struct PhoneNumber {
+        area_code: Option<u8>,
+        number: u32,
+    }
+    impl Person {
+        // 获取此人的工作电话号码的区号,
+        fn work_phone_area_code(&self) -> Option<u8> {
+            // 没有? 运算符的话 这将徐亚很多嵌套的 match 语句
+            // 这将需要更多代码 尝试自己编写一下 看看那个更容易
+            self.job?.phone_number?.area_code
+        }
+    }
+    let p = Person {
+        job: Some(Job {
+            phone_number: Some(PhoneNumber {
+                area_code: Some(61),
+                number: 4242423,
+            }),
+        }),
+    };
+    assert_eq!(p.work_phone_area_code(), Some(61));
+    // ===================== 错误处理 -> 组合算子：map ===================
+    #[derive(Debug)]
+    enum Food {
+        Apple,
+        Carrot,
+        Potato,
+    }
+    #[derive(Debug)]
+    struct Peeled(Food);
+
+    #[derive(Debug)]
+    struct Chopped(Food);
+
+    #[derive(Debug)]
+    struct Cooked(Food);
+
+    //削皮 如果 没有食物, 就是返回 None 否则返回削好皮的食物
+    fn peel(food: Option<Food>) -> Option<Peeled> {
+        match food {
+            Some(food) => Some(Peeled(food)),
+            None => None,
+        }
+    }
+
+    // 切食物 如果没有食物, 就返回none 否则返回切好的食物
+    fn chop(peeled: Option<Peeled>) -> Option<Chopped> {
+        match peeled {
+            Some(Peeled(food)) => Some(Chopped(food)),
+            None => None,
+        }
+    }
+
+    // 亨饪食物, 这里 我们使用 map 来代替 match 以处理 各种情况
+    fn cook(chopped: Option<Chopped>) -> Option<Cooked> {
+        chopped.map(|Chopped(food)| Cooked(food))
+    }
+
+    // 这个函数会完成削皮切换快亨饪一条龙 我们吧 map 串起来 以简化代码
+    fn process(food: Option<Food>) -> Option<Cooked> {
+        food.map(|f| Peeled(f))
+            .map(|Peeled(f)| Chopped(f))
+            .map(|Chopped(f)| Cooked(f))
+    }
+
+    // 再尝试吃食物之前确认食物是否存在时非常重要的事
+    fn eat(food: Option<Cooked>) {
+        match food {
+            Some(food) => println!("Mnn. I love {:?}", food),
+            None => println!("Oh no! It wasn't editable."),
+        }
+    }
+
+    let apple = Some(Food::Apple);
+    let carrot = Some(Food::Carrot);
+    let potato = None;
+
+    let cooked_apple = cook(chop(peel(apple)));
+    let cooked_carrot = cook(chop(peel(carrot)));
+
+    // 现在让我们试试看起来更简单的 process
+    let cooked_potato = process(potato);
+
+    eat(cooked_apple);
+    eat(cooked_potato);
+    eat(cooked_carrot);
+
+    // ===================== 错误处理 -> 组合算子：and_then ===================
+    #[derive(Debug)]
+    enum Food2 {
+        CordonBleu,
+        Steak,
+        Sushi,
+    }
+    #[derive(Debug)]
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+    }
+
+    // 我们没有制作寿司所需要的原材料 (ingredient) 有其他的原材料
+    fn have_ingredients(food: Food2) -> Option<Food2> {
+        match food {
+            Food2::Sushi => None,
+            _ => Some(food),
+        }
+    }
+
+    // 我们拥有全部的食物的食谱, 除了法国蓝带猪排 (Cordon Bleu) 的
+    fn have_recipe(food: Food2) -> Option<Food2> {
+        match food {
+            Food2::CordonBleu => None,
+            _ => Some(food),
+        }
+    }
+
+    // 要做一份好菜, 我们需要原材料和食谱
+    // 我们可以借助一系列 match 来表达这个逻辑
+    fn cookable_v1(food: Food2) -> Option<Food2> {
+        match have_ingredients(food) {
+            None => None,
+            Some(food) => match have_recipe(food) {
+                None => None,
+                Some(food) => Some(food),
+            },
+        }
+    }
+
+    // 也可以借用 and_then 把上面的逻辑改写的更紧凑点
+    fn cookable_v2(food: Food2) -> Option<Food2> {
+        have_ingredients(food).and_then(have_recipe)
+    }
+
+    fn eat2(food: Food2, day: Day) {
+        match cookable_v2(food) {
+            Some(food) => println!("Yay! On {:?}, we get to eat {:?}.", day, food),
+            None => println!("Oh no. we don't get to eat on {:?}?", day),
+        }
+    }
+
+    let (cordon_bleu, steak, sushi) = (Food2::CordonBleu, Food2::Steak, Food2::Sushi);
+
+    eat2(cordon_bleu, Day::Monday);
+    eat2(steak, Day::Tuesday);
+    eat2(sushi, Day::Wednesday);
 }
+use core::panic;
 // ==================macro_rules -> DRY (不写重复代码)=========
-use std::ops::{Add, Mul, Sub};
+use std::{
+    fmt::format,
+    ops::{Add, Mul, Sub},
+};
 macro_rules! assert_equal_len {
     ($a:ident, $b: ident, $func:ident, $op:tt) => {
         assert!(
